fbed2f63378a94b72917e6a2bcad98995f47f678
Akisanya Temiloluwa <tolutech2004@gmail.com>
2024-03-16 17:43:33 +0100

    Initial Commit

---
+++ Remove Background/Remove_BackgroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  Remove_BackgroundApp.swift
+//  Remove Background
+//
+//  Created by Temiloluwa Akisanya on 16/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct Remove_BackgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
