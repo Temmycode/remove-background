073f0b4cfa326fa8fe7a5985c4c7cdf9a2f236cf
Akisanya Temiloluwa <tolutech2004@gmail.com>
2024-03-18 14:25:37 +0100

    Save image laptop 😉

--- Remove Background/HomeView.swift
+++ Remove Background/HomeView.swift
@@ -10,6 +10,7 @@ import AppKit
 
 struct HomeView: View {
     @Environment(Repository.self) var repositoryProvider
+    @State private var isShowingSaveToFinder = false
     @State private var selectedImage: NSImage? = nil
     @State private var fileName: String?
     @State private var receivedURLs: [URL] = []
@@ -49,6 +50,21 @@ struct HomeView: View {
                 }
                 .foregroundStyle(.black)
             }
+            .toolbar {
+                Button("download", systemImage: "square.and.arrow.down", action: {
+                    if let image = repositoryProvider.removedBackgroundImage {
+                        saveImageToFinder(image: image) { success in
+                            if success {
+                                print("Successfully saved")
+                            } else {
+                                print("Not successfully saved")
+                            }
+                        }
+                    } else {
+                        print("Have diginity gee")
+                    }
+                })
+            }
             .frame(maxWidth: .infinity, maxHeight: .infinity)
             .onReceive(NotificationCenter.default.publisher(for: .receivedUrlsNotification), perform: { notification in
                 if let image = notification.userInfo?["URLs"] as? NSImage {
@@ -81,6 +97,31 @@ struct HomeView: View {
             if let selectedUrl = openPanel.url, let image = NSImage(contentsOfFile: selectedUrl.path) {
                 fileName = selectedUrl.lastPathComponent
                 selectedImage = image
+            }
+        }
+    }
+    
+    func saveImageToFinder(image: NSImage, completion: @escaping (Bool) -> Void) {
+        let savePanel = NSSavePanel()
+        savePanel.allowedContentTypes = [.png, .jpeg] // Allow only PNG format
+        savePanel.canCreateDirectories = true // Allow creating folders
+        
+        savePanel.begin { (result) in
+            if result == .OK, let url = savePanel.url {
+                if let imageData = repositoryProvider.nsPngData(image: image) {
+                    do {
+                        try imageData.write(to: url, options: .atomic)
+                        completion(true) // Successful save
+                    } catch {
+                        print("Error saving image: \(error)")
+                        completion(false) // Error saving
+                    }
+                } else {
+                    print("Failed to convert image to PNG data")
+                    completion(false) // Error converting to PNG
+                }
+            } else {
+                completion(false) // User canceled saving
             }
         }
     }
